962d4c4b1947a287f10ac2481a757cd89e600ad7
Lian <liana.khachatryan1999@mail.ru>
2019-09-13 18:40:03 +0400

    Second commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string name;
+            int age;
+
+            Console.Write("your name: ");
+            name =Console.ReadLine();
+            Console.Write("Your age: ");
+            age = Int32.Parse(Console.ReadLine());
+        
+        Console.WriteLine( $"Your name {name}. You were born at {2019 - age}.");
+    }
+  }
+}
